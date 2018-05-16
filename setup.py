3d270df033a69f7bee1a3c949a1374ebccc26000
Lee YongJin <jjeaby@gmail.com>
2018-05-17 00:17:46 +0900

    first update

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+install_requires = [
+    "pillow",
+]
+
+setup(
+    name='JPhotoWorks',
+    version='0.04',
+    py_modules = ['JPhoto','JUtil'],
+    packages=find_packages(exclude=['tests']),
+    install_requires=install_requires,
+    include_package_data=True,
+    url='https://github.com/jjeaby/JPhotoWorks',
+    license='Apache 2.0',
+    author='Lee Yong Jin',
+    author_email='jjeaby@gmail.com',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    classifiers      = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
